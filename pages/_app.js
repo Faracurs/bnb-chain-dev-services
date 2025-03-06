00c75ddd2935887095f248378dce4035007e1982
Faracurs <aivaras.bardauskas@gmail.com>
2025-03-07 01:54:29 +0700

    Add minimal _app.js file to pages directory

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+// This is a minimal _app.js file to satisfy the Next.js build process
+// The actual application uses the App Router in src/app
+
+import React from 'react';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
