5f9b72fb1eb0147484fd4aed7768bf91e00837e4
Faracurs <aivaras.bardauskas@gmail.com>
2025-03-06 23:06:49 +0700

    Add legacy page to avoid conflict with app directory

---
+++ pages/legacy.js
@@ -0,0 +1,7 @@
+// This file is intentionally empty to satisfy the build requirement
+// The actual application uses the App Router in src/app
+// This file will not be used in production as the app directory takes precedence
+
+export default function LegacyHome() {
+  return null;
+}
