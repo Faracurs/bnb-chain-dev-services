12baa643c89764c7d2e39109c454e67a1e642f95
Faracurs <aivaras.bardauskas@gmail.com>
2025-03-07 01:08:56 +0700

    Remove _app.js file

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-
-// This file exists to satisfy the build requirement
-// The actual application uses the App Router in src/app
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
