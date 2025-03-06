4d88ef8fa3229a427b0feead369ef8c6fe8f3d81
Faracurs <aivaras.bardauskas@gmail.com>
2025-03-07 01:38:18 +0700

    Add components index file to fix build error

---
+++ src/app/components/index.tsx
@@ -0,0 +1,13 @@
+// Export all components from this file
+// This helps resolve the "Error: page componentndex.tsx not found" error
+
+export { default as Navbar } from './Navbar';
+export { default as Hero } from './Hero';
+export { default as Services } from './Services';
+export { default as TokenDisplay } from './TokenDisplay';
+export { default as Web3Integration } from './Web3Integration';
+export { default as NFTMinter } from './NFTMinter';
+export { default as ContactForm } from './ContactForm';
+export { default as Footer } from './Footer';
+export { default as LoadingState } from './LoadingState';
+export { default as ErrorBoundary } from './ErrorBoundary';
